432875ebc706cda949c7c90d35ab5c77576722f9
Hoangjunss <109050903+Hoangjunss@users.noreply.github.com>
2023-10-14 15:47:46 +0700

    Update data.js

--- javascripts/data.js
+++ javascripts/data.js
@@ -177,5 +177,185 @@ var list_products =[{
         "battery": "5000 mAh"
     },
     "masp":"xiao010"
+},
+{
+    "name": "Vivo Y33",
+    "company": "Vivo",
+    "img": "image/Vivo_Y33s.jpg",
+    "price": "5.140.000",
+    "listprice":"0-5milion",
+    "detail": {
+        "screen": "IPS LCD FULl HD+",
+        "os": "Android 11",
+        "camera": "50 Mp & 2 Mp, 2 Mp",
+        "cameraFront": "16 Mp",
+        "cpu": "MediaTek Helio G80 8 nhân",
+        "ram": "8GB",
+        "rom": "128GB",
+        "battery":"5000mAh"
+    },
+    "masp": "vivo01"
+},
+{
+    "name": "Vivo Y17s 4G",
+    "company": "Vivo",
+    "img": "image/Vivo_Y17s_4G.jpg",
+    "price": "3.790.000",
+    "listprice":"0-5milion",
+    "detail": {
+        "screen": "IPS LCD HD+",
+        "os": "Android 11",
+        "camera": "Chính 50 MP & Phụ 2 MP",
+        "cameraFront": "8 MP",
+        "cpu": "MediaTek Helio G85 8 nhân",
+        "ram": "4 GB",
+        "rom": "128 GB",
+        "battery":"5000mAh"
+    },
+    "masp": "vivo02"
+},
+{
+    "name": "Vivo V25 5G",
+    "company": "Vivo",
+    "img": "image/vivo-v25-vang-1.jpg",
+    "price": "6.990.000",
+    "listprice":"0-5milion",
+    "detail": {
+        "screen": "AMOLED Full HD+",
+        "os": "Android 12",
+        "camera": "Chính 64 MP & Phụ 8 MP, 2 MP",
+        "cameraFront": "50 MP",
+        "cpu": "MediaTek Dimensity 900 5G",
+        "ram": "8 GB",
+        "rom": "128 GB",
+        "battery":"4500mAh"
+    },
+    "masp": "vivo03"
+},
+{
+    "name": "Vivo V25 Pro 5G",
+    "company": "Vivo",
+    "img": "image/vivo-v25-pro-5g-sld-xanh-1.jpg",
+    "price": "9.990.000",
+    "listprice":"0-5milion",
+    "detail": {
+        "screen": "AMOLED Full HD+",
+        "os": "Android 12",
+        "camera": "Chính 64 MP & Phụ 8 MP, 2 MP",
+        "cameraFront": "32 MP",
+        "cpu": "MediaTek Dimensity 1300 8 nhân",
+        "ram": "8 GB",
+        "rom": "128 GB",
+        "battery":"4830mAh"
+    },
+    "masp": "vivo04"
+},
+{
+    "name": "Vivo V27e",
+    "company": "Vivo",
+    "img": "image/vivo-v27e-tim-1-1.jpg",
+    "price": "8.190.000",
+    "listprice":"0-5milion",
+    "detail": {
+        "screen": "AMOLED Full HD+",
+        "os": "Android 13",
+        "camera": "Chính 64 MP & Phụ 8 MP, 2 MP",
+        "cameraFront": "32 MP",
+        "cpu": "MediaTek Helio G99",
+        "ram": "8 GB",
+        "rom": "256 GB",
+        "battery":"4600mAh"
+    },
+    "masp": "vivo05"
+},
+{
+    "name": "Vivo Y36",
+    "company": "Vivo",
+    "img": "image/vivo-y36-xanh-glr-1.jpeg",
+    "price": "5.790.000",
+    "listprice":"0-5milion",
+    "detail": {
+        "screen": "IPS LCD Full HD+",
+        "os": "Android 13",
+        "camera": "Chính 50 MP & Phụ 2 MP",
+        "cameraFront": "16 MP",
+        "cpu": "Snapdragon 680",
+        "ram": "8 GB",
+        "rom": "128 GB",
+        "battery":"5000mAh"
+    },
+    "masp": "vivo06"
+},
+{
+    "name": "Vivo Y35",
+    "company": "Vivo",
+    "img": "image/vivo-y35-den-1-1.jpg",
+    "price": "4.590.000",
+    "listprice":"0-5milion",
+    "detail": {
+        "screen": "IPS LCD Full HD+",
+        "os": "Android 12",
+        "camera": "Chính 50 MP & Phụ 2 MP, 2 MP",
+        "cameraFront": "16 MP",
+        "cpu": "Snapdragon 680",
+        "ram": "8 GB",
+        "rom": "128 GB",
+        "battery":"5000mAh"
+    },
+    "masp": "vivo07"
+},
+{
+    "name": "Vivo Y22s 4GB",
+    "company": "Vivo",
+    "img": "image/vivo-y22s-vang-1.jpg",
+    "price": "3.690.000",
+    "listprice":"0-5milion",
+    "detail": {
+        "screen": "IPS LCD Full HD+",
+        "os": "Android 12",
+        "camera": "Chính 50 MP & Phụ 2 MP",
+        "cameraFront": "8 MP",
+        "cpu": "Snapdragon 680",
+        "ram": "4 GB",
+        "rom": "128 GB",
+        "battery":"5000mAh"
+    },
+    "masp": "vivo08"
+},
+{
+    "name": "Vivo Y16",
+    "company": "Vivo",
+    "img": "image/vivo-y16-vang-1.jpg",
+    "price": "2.990.000",
+    "listprice":"0-5milion",
+    "detail": {
+        "screen": "IPS LCD HD+",
+        "os": "Android 12",
+        "camera": "Chính 13 MP & Phụ 2 MP",
+        "cameraFront": "5 MP",
+        "cpu": "MediaTek Helio P35",
+        "ram": "4 GB",
+        "rom": "64 GB",
+        "battery":"5000mAh"
+    },
+    "masp": "vivo09"
+},
+{
+    "name": "Vivo Y02T",
+    "company": "Vivo",
+    "img": "image/vivo-y02t-xanh-tim-1.jpeg",
+    "price": "2.990.000",
+    "listprice":"0-5milion",
+    "detail": {
+        "screen": "IPS LCD HD+",
+        "os": "Android 13",
+        "camera": "8 MP",
+        "cameraFront": "5 MP",
+        "cpu": "MediaTek Helio P35",
+        "ram": "4 GB",
+        "rom": "64 GB",
+        "battery":"5000mAh"
+    },
+    "masp": "vivo010"
 }
 ]
