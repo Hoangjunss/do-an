3d1ec2f63640a099586f00795cee406fb4dfa003
Hoangjunss <109050903+Hoangjunss@users.noreply.github.com>
2023-11-03 07:56:08 +0700

    1

--- javascripts/seaarch.js
+++ javascripts/seaarch.js
@@ -1,4 +1,7 @@
 const categories = [...new Set(list_products.map((item) => { return item }))]
+var filteredData=categories;
+const itemsPerPage = 5;
+let currentPage = 1;
 
 const displayItem = (items) => {
     
@@ -21,16 +24,16 @@ const displayItem = (items) => {
         )
     }).join('')
 };
-displayItem(categories);
+displayProductsOnPage(currentPage);
 
 document.getElementById('searchBar').addEventListener('keyup', (e) => {
     const searchData = e.target.value.toLowerCase();
-    const filteredData = categories.filter((item) => {
+     filteredData = categories.filter((item) => {
         return (
             item.name.toLowerCase().includes(searchData)
         )
     })
-    displayItem(filteredData)
+    displayProductsOnPage(currentPage);
 });
 /*function getcompany(){
      const searchData = document.getElementById('list-company').value.toLowerCase();
@@ -62,83 +65,74 @@ function getsearch(){
     
     if(searchDatacompany!="company" && searchDataprice!="price" && searchDataram!="ram"){
         console.log("1");
-        const filteredData = categories.filter((item) => {
+         filteredData = categories.filter((item) => {
             return (
                  item.company.includes(searchDatacompany)&& item.listprice && item.listprice.includes(searchDataprice)&& item.detail.ram && item.detail.ram.includes(searchDataram)
             )
         })
         
-        displayItem(filteredData)
+        displayProductsOnPage(currentPage);
     }else
     if(searchDataprice && searchDatacompany=="company"&&searchDataram=="ram"){
         console.log("2");
-        const filteredData = categories.filter((item) => {
+         filteredData = categories.filter((item) => {
             return (
                item.listprice && item.listprice.includes(searchDataprice)
             )
         })
         
-        displayItem(filteredData)
+        displayProductsOnPage(currentPage);
     }else if(searchDatacompany &&searchDataram=="ram"&&searchDataprice=="price" ){
         console.log("3");
-            const filteredData = categories.filter((item) => {
+             filteredData = categories.filter((item) => {
                 return (
                     item.company.includes(searchDatacompany)
                 )
             })
             console.log(filteredData)
-            displayItem(filteredData)
+            
+           // displayItem(filteredData)
         }else if(searchDataprice && searchDatacompany!="company"&&searchDataram=="ram"){
-            const filteredData = categories.filter((item) => {
+             filteredData = categories.filter((item) => {
                 return (
                     item.company.includes(searchDatacompany)&& item.listprice && item.listprice.includes(searchDataprice)
                 )
             })
             
-            displayItem(filteredData)
+            displayProductsOnPage(currentPage);
         }
         else if(searchDataprice=="price" && searchDatacompany=="company"&&searchDataram!="ram"){
-            const filteredData = categories.filter((item) => {
+             filteredData = categories.filter((item) => {
                 return (
                     item.detail.ram && item.detail.ram.includes(searchDataram)
                 )
             })
-            displayItem(filteredData)
+            displayProductsOnPage(currentPage);
         }
         else if(searchDataprice!="price" && searchDatacompany=="company"&&searchDataram!="ram"){
             console.log("price ram")
             console.log(searchDataprice);
             console.log(searchDataram)
-            const filteredData = categories.filter((item) => {
+             filteredData = categories.filter((item) => {
                 return (
                     item.listprice && item.listprice.includes(searchDataprice)&& item.detail.ram && item.detail.ram.includes(searchDataram)
                 )
             })
             console.log(filteredData)
-            displayItem(filteredData)
+            displayProductsOnPage(currentPage);
         }else if(searchDataprice=="price" && searchDatacompany!="company"&&searchDataram!="ram"){
             console.log(5)
-            const filteredData = categories.filter((item) => {
+             filteredData = categories.filter((item) => {
                 return (
                    item.company&& item.company.includes(searchDatacompany)&& item.detail.ram && item.detail.ram.includes(searchDataram)
                 )
             })
-            displayItem(filteredData)
+            displayProductsOnPage(currentPage);
         }
     
 }
 
-/*function getma(e){
-    e.preventDefault();
-    const searchData = e.querySelector('.name');
-    console.log(searchData);
-    const filteredData = categories.filter((item) => {
-        return (
-            item.listprice.toLowerCase().includes(searchData)
-        )
-    })
-    displayItem(filteredData)
-}*/
+
 function them(masp,name){
     console.log(name);
 }
@@ -146,13 +140,47 @@ function getram(){
     searchDataram=document.getElementById('list-ram').value;
     console.log(searchDataram);
     if(searchDataram){
-        const filteredData = categories.filter((item) => {
+         filteredData = categories.filter((item) => {
             return (
                 item.detail.ram && item.detail.ram.includes(searchDataram)
             )
         })
         console.log(filteredData)
-        displayItem(filteredData)
+        console.log(filteredData.slice(0,5))
+        displayProductsOnPage(currentPage);
     }
 }
+
+function displayProductsOnPage(pageNumber) {
+    
+    const startIndex = (pageNumber - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    const productsToDisplay = filteredData.slice(startIndex, endIndex);
+    displayItem(productsToDisplay);
+}
+document.getElementById('nextPage').addEventListener('click', () => {
+    if (currentPage * itemsPerPage < filteredData.length) {
+        currentPage++;
+        displayProductsOnPage(currentPage);
+        updatePaginationButtons();
+    }
+});
+
+document.getElementById('prevPage').addEventListener('click', () => {
+    if (currentPage > 1) {
+        currentPage--;
+        displayProductsOnPage(currentPage);
+        updatePaginationButtons();
+    }
+});
+function updatePaginationButtons() {
+    const prevPageButton = document.getElementById('prevPage');
+    const nextPageButton = document.getElementById('nextPage');
+
+    prevPageButton.disabled = currentPage === 1;
+    nextPageButton.disabled = currentPage * itemsPerPage >= list_products.length;
+}
+
+// Initialize pagination buttons
+updatePaginationButtons();
 
